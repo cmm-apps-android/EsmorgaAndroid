33400ab2f62f0c42565c279b3ac812f972b4170b
Ramos, Yago <138661338+YagoAccenture@users.noreply.github.com>
2025-05-15 13:05:37 +0200

    Fixed alert dialog

--- view/src/main/java/cmm/apps/esmorga/view/profile/ProfileView.kt
+++ view/src/main/java/cmm/apps/esmorga/view/profile/ProfileView.kt
@@ -3,6 +3,7 @@ package cmm.apps.esmorga.view.profile
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,10 +11,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -22,9 +26,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -145,15 +152,24 @@ private fun LoggedProfileView(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        EsmorgaText(text = stringResource(R.string.my_profile_name), style = EsmorgaTextStyle.HEADING_1)
+        EsmorgaText(
+            text = stringResource(R.string.my_profile_name),
+            style = EsmorgaTextStyle.HEADING_1
+        )
         EsmorgaText(text = "$name $lastName", style = EsmorgaTextStyle.BODY_1)
         Spacer(modifier = Modifier.height(52.dp))
 
-        EsmorgaText(text = stringResource(R.string.my_profile_email), style = EsmorgaTextStyle.HEADING_1)
+        EsmorgaText(
+            text = stringResource(R.string.my_profile_email),
+            style = EsmorgaTextStyle.HEADING_1
+        )
         EsmorgaText(text = email, style = EsmorgaTextStyle.BODY_1)
         Spacer(modifier = Modifier.height(52.dp))
 
-        EsmorgaText(text = stringResource(R.string.my_profile_options), style = EsmorgaTextStyle.HEADING_1)
+        EsmorgaText(
+            text = stringResource(R.string.my_profile_options),
+            style = EsmorgaTextStyle.HEADING_1
+        )
         Spacer(modifier = Modifier.height(16.dp))
 
         Row(
@@ -166,7 +182,10 @@ private fun LoggedProfileView(
                     onChangePassword()
                 }
         ) {
-            EsmorgaText(text = stringResource(R.string.my_profile_changue_password), style = EsmorgaTextStyle.HEADING_2)
+            EsmorgaText(
+                text = stringResource(R.string.my_profile_changue_password),
+                style = EsmorgaTextStyle.HEADING_2
+            )
 
             Icon(
                 imageVector = Icons.AutoMirrored.Filled.ArrowForward,
@@ -182,7 +201,10 @@ private fun LoggedProfileView(
                 .padding(vertical = 16.dp)
                 .clickable { shownDialog = true }
         ) {
-            EsmorgaText(text = stringResource(R.string.my_profile_logout), style = EsmorgaTextStyle.HEADING_2)
+            EsmorgaText(
+                text = stringResource(R.string.my_profile_logout),
+                style = EsmorgaTextStyle.HEADING_2
+            )
 
             Icon(
                 imageVector = Icons.AutoMirrored.Filled.ArrowForward,
@@ -194,28 +216,59 @@ private fun LoggedProfileView(
 
 @Composable
 fun LogoutDialog(onConfirm: () -> Unit, onDismiss: () -> Unit) {
+    val primaryColor = colorResource(id = R.color.primary)
+    val secondaryColor = colorResource(id = R.color.secondary)
+
     AlertDialog(
         onDismissRequest = onDismiss,
-        confirmButton = {
+        containerColor = Color.White,
+
+        title = {
             EsmorgaText(
-                text = stringResource(R.string.my_profile_logout_pop_up_confirm),
+                text = stringResource(R.string.my_profile_logout_pop_up_title),
                 style = EsmorgaTextStyle.BODY_1,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        },
+
+        text = null,
+
+        dismissButton = {
+            Box(
                 modifier = Modifier
+                    .background(secondaryColor, shape = RoundedCornerShape(6.dp))
+                    .clickable { onDismiss() }
+                    .padding(vertical = 8.dp, horizontal = 24.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stringResource(R.string.my_profile_logout_pop_up_cancel),
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = Color.Black
+                )
+            }
+        },
+
+        confirmButton = {
+            Box(
+                modifier = Modifier
+                    .background(primaryColor, shape = RoundedCornerShape(6.dp))
                     .clickable {
                         onDismiss()
                         onConfirm()
                     }
-                    .padding(start = 10.dp)
-            )
-        },
-        dismissButton = {
-            EsmorgaText(
-                text = stringResource(R.string.my_profile_logout_pop_up_cancel),
-                style = EsmorgaTextStyle.BODY_1,
-                modifier = Modifier.clickable { onDismiss() }
-            )
-        },
-        title = { EsmorgaText(text = stringResource(R.string.my_profile_logout_pop_up_title), style = EsmorgaTextStyle.TITLE) }
+                    .padding(vertical = 8.dp, horizontal = 24.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stringResource(R.string.my_profile_logout_pop_up_confirm),
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = Color.White
+                )
+            }
+        }
+
     )
 }
 
