c73a5551f5363faabbacd1d99df5bd75510a069a
Pablo Mariño Cortés <pablomarinoacn@gmail.com>
2025-05-26 11:03:10 +0200

    added error screens and navgations

--- view/src/main/java/cmm/apps/esmorga/view/activateaccount/ActivateAccountViewModel.kt
+++ view/src/main/java/cmm/apps/esmorga/view/activateaccount/ActivateAccountViewModel.kt
@@ -1,5 +1,7 @@
 package cmm.apps.esmorga.view.activateaccount
 
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import cmm.apps.esmorga.domain.account.ActivateAccountUseCase
@@ -17,32 +19,35 @@ import kotlinx.coroutines.launch
 class ActivateAccountViewModel(
     private val verificationCode: String,
     private val activateAccountUseCase: ActivateAccountUseCase
-) : ViewModel() {
+) : ViewModel(), DefaultLifecycleObserver {
 
     private val _uiState = MutableStateFlow(ActivateAccountUiState())
     val uiState: StateFlow<ActivateAccountUiState> = _uiState.asStateFlow()
 
-    private val _effect = MutableSharedFlow<ActivateAccountEffect>(
-        extraBufferCapacity = 1,
-        onBufferOverflow = BufferOverflow.DROP_OLDEST
-    )
+    private val _effect = MutableSharedFlow<ActivateAccountEffect>(extraBufferCapacity = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
     val effect: SharedFlow<ActivateAccountEffect> = _effect.asSharedFlow()
 
     private var failedAttempts = 0
 
-    init {
+    override fun onStart(owner: LifecycleOwner) {
+        super.onStart(owner)
         activateAccount(verificationCode)
     }
 
-    fun activateAccount(verificationCode: String) {
-
+    private fun activateAccount(verificationCode: String) {
         viewModelScope.launch {
             val result = activateAccountUseCase(verificationCode)
 
             result.onSuccess {
                 _uiState.value = ActivateAccountUiState(isLoading = false)
             }.onFailure {
-                _effect.tryEmit(ActivateAccountEffect.ShowFullScreenError())
+                if (failedAttempts < 2) {
+                    failedAttempts++
+                    _effect.tryEmit(ActivateAccountEffect.ShowFullScreenError())
+                } else {
+                    _effect.tryEmit(ActivateAccountEffect.ShowLastTryFullScreenError())
+                }
+
             }
         }
     }
