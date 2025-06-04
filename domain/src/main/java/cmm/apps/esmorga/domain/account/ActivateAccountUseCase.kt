6e05c08770faae439f955e97687a18bfe338edc1
Ramos, Yago <138661338+YagoAccenture@users.noreply.github.com>
2025-06-04 09:08:45 +0200

    Corrected comments

--- domain/src/main/java/cmm/apps/esmorga/domain/account/ActivateAccountUseCase.kt
+++ domain/src/main/java/cmm/apps/esmorga/domain/account/ActivateAccountUseCase.kt
@@ -4,16 +4,16 @@ import cmm.apps.esmorga.domain.result.EsmorgaResult
 import cmm.apps.esmorga.domain.user.repository.UserRepository
 
 interface ActivateAccountUseCase {
-    suspend operator fun invoke(code: String): EsmorgaResult<Unit>
+    suspend operator fun invoke(verificationCode: String): EsmorgaResult<Unit>
 }
 
 class ActivateAccountUseCaseImpl(
     private val repo: UserRepository
 ) : ActivateAccountUseCase {
 
-    override suspend fun invoke(code: String): EsmorgaResult<Unit> {
+    override suspend fun invoke(verificationCode: String): EsmorgaResult<Unit> {
         return try {
-            repo.activateAccount(code)
+            repo.activateAccount(verificationCode)
             EsmorgaResult.success(Unit)
         } catch (e: Exception) {
             EsmorgaResult.failure(e)
