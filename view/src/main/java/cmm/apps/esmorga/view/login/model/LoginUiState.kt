198333f5d6f71c2e94803c331081d2de47efa32c
MercedesRegueiroRojas <m.regueiro.rojas@accenture.com>
2025-06-06 13:33:40 +0200

    Add test

--- view/src/main/java/cmm/apps/esmorga/view/login/model/LoginUiState.kt
+++ view/src/main/java/cmm/apps/esmorga/view/login/model/LoginUiState.kt
@@ -21,7 +21,7 @@ sealed class LoginEffect {
     data object NavigateToEventList : LoginEffect()
     data class ShowFullScreenError(val esmorgaErrorScreenArguments: EsmorgaErrorScreenArguments = getEsmorgaErrorScreenArguments()) : LoginEffect()
     data object NavigateToForgotPassword : LoginEffect()
-    data object ShowInitSnackbar : LoginEffect()
+    data class ShowInitSnackbar(val message: String) : LoginEffect()
 }
 
 object LoginViewHelper : KoinComponent{
