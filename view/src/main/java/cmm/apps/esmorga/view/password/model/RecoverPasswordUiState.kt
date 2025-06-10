b95b6fb1d59467095a4f2b30577e96b59abd1e74
Pablo Mariño Cortés <pablomarinoacn@gmail.com>
2025-06-10 09:55:24 +0200

    reusable default arguments

--- view/src/main/java/cmm/apps/esmorga/view/password/model/RecoverPasswordUiState.kt
+++ view/src/main/java/cmm/apps/esmorga/view/password/model/RecoverPasswordUiState.kt
@@ -3,7 +3,7 @@ package cmm.apps.esmorga.view.password.model
 import android.content.Context
 import cmm.apps.esmorga.view.R
 import cmm.apps.esmorga.view.errors.model.EsmorgaErrorScreenArguments
-import cmm.apps.esmorga.view.password.model.RecoverPasswordViewHelper.getEsmorgaErrorScreenArguments
+import cmm.apps.esmorga.view.errors.model.EsmorgaErrorScreenArgumentsHelper.getEsmorgaDefaultErrorScreenArguments
 import cmm.apps.esmorga.view.password.model.RecoverPasswordViewHelper.getEsmorgaMessageSnackBarSuccess
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -16,15 +16,11 @@ data class RecoverPasswordUiState(
 
 sealed class RecoverPasswordEffect {
     data class ShowSnackbarSuccess(val message: String = getEsmorgaMessageSnackBarSuccess()) : RecoverPasswordEffect()
-    data class ShowFullScreenError(val esmorgaErrorScreenArguments: EsmorgaErrorScreenArguments = getEsmorgaErrorScreenArguments()) : RecoverPasswordEffect()
+    data class ShowFullScreenError(val esmorgaErrorScreenArguments: EsmorgaErrorScreenArguments = getEsmorgaDefaultErrorScreenArguments()) : RecoverPasswordEffect()
 }
 
 object RecoverPasswordViewHelper : KoinComponent {
     private val context: Context by inject()
     fun getEsmorgaMessageSnackBarSuccess() = context.getString(R.string.forgot_password_snackbar_success)
     fun getEmailErrorText() = context.getString(R.string.invalid_credentials_error)
-    fun getEsmorgaErrorScreenArguments() = EsmorgaErrorScreenArguments(
-        title = context.getString(R.string.default_error_title_expanded),
-        buttonText = context.getString(R.string.button_retry)
-    )
 }
