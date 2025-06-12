31d5bf0e53c7437edaf4f13f29e341cf0c3da1e2
Ramos, Yago <138661338+YagoAccenture@users.noreply.github.com>
2025-06-12 09:27:48 +0200

    Unnecessary comments and imports removed

--- view/src/main/java/cmm/apps/esmorga/view/profile/ProfileView.kt
+++ view/src/main/java/cmm/apps/esmorga/view/profile/ProfileView.kt
@@ -1,23 +1,17 @@
 package cmm.apps.esmorga.view.profile
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
@@ -160,11 +154,7 @@ private fun LoggedProfileView(
             style = EsmorgaTextStyle.HEADING_1, modifier = Modifier.padding(bottom = 16.dp)
         )
 
-
-        // The following code is commented out due to release 1, in which the change password functionality is not implemented yet, and that's why we hide the button from the user.
-        //EsmorgaRow(title = stringResource(R.string.my_profile_changue_password), modifier = Modifier.clickable { onChangePassword() } )
-
-        EsmorgaRow(title = stringResource(R.string.my_profile_logout), modifier = Modifier.clickable { shownDialog = true } )
+        EsmorgaRow(title = stringResource(R.string.my_profile_logout), modifier = Modifier.clickable { shownDialog = true })
     }
 }
 
