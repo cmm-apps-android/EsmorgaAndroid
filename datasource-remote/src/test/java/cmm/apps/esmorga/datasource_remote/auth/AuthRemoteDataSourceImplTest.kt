18f5435712bc9083a5762b83b02aa9deaaffcf7f
Pablo Mariño Cortés <pablomarinoacn@gmail.com>
2025-07-15 13:59:39 +0200

    added some tests

---
+++ datasource-remote/src/test/java/cmm/apps/esmorga/datasource_remote/auth/AuthRemoteDataSourceImplTest.kt
@@ -0,0 +1,64 @@
+package cmm.apps.esmorga.datasource_remote.auth
+
+import android.content.SharedPreferences
+import cmm.apps.esmorga.datasource_remote.api.EsmorgaAuthApi
+import cmm.apps.esmorga.datasource_remote.user.AuthRemoteDatasourceImpl
+import io.mockk.coEvery
+import io.mockk.mockk
+import io.mockk.slot
+import org.junit.Assert
+import org.junit.Test
+
+class AuthRemoteDataSourceImplTest {
+    private var fakeToken: String? = null
+    private var fakeRefreshToken: String? = null
+    private var fakeExpirationDate: Long = 0
+
+    private fun provideFakeSharedPreferences(): SharedPreferences {
+        val fakeTokenSlot = slot<String>()
+        val fakeRefreshTokenSlot = slot<String>()
+        val fakeExpirationDateSlot = slot<Long>()
+        val sharedPreferences = mockk<SharedPreferences>(relaxed = true)
+        coEvery { sharedPreferences.getString("access_token", null) } coAnswers {
+            fakeToken
+        }
+        coEvery { sharedPreferences.getString("refresh_token", null) } coAnswers {
+            fakeRefreshToken
+        }
+        coEvery { sharedPreferences.getLong("token_expiration_date", 0) } coAnswers {
+            fakeExpirationDate
+        }
+        coEvery { sharedPreferences.edit().putString("access_token", capture(fakeTokenSlot)).apply() } coAnswers {
+            fakeToken = fakeTokenSlot.captured
+        }
+        coEvery { sharedPreferences.edit().putString("refresh_token", capture(fakeRefreshTokenSlot)).apply() } coAnswers {
+            fakeRefreshToken = fakeRefreshTokenSlot.captured
+        }
+        coEvery { sharedPreferences.edit().putLong("token_expiration_date", capture(fakeExpirationDateSlot)).apply() } coAnswers {
+            fakeExpirationDate = fakeExpirationDateSlot.captured
+        }
+        return sharedPreferences
+    }
+
+    @Test
+    fun `given an empty storage when access token is cached then get access token stored successfully`() {
+        fakeToken = "3883hh8fhfh84fh489"
+
+        val api = mockk<EsmorgaAuthApi>(relaxed = true)
+        val sut = AuthRemoteDatasourceImpl(api, provideFakeSharedPreferences())
+        val result = sut.getAccessToken()
+
+        Assert.assertEquals(fakeToken, result)
+    }
+
+    @Test
+    fun `given an empty storage when refresh token is cached then get refresh token stored successfully`() {
+        fakeRefreshToken = "3883hh8fhfh84fh489"
+
+        val api = mockk<EsmorgaAuthApi>(relaxed = true)
+        val sut = AuthRemoteDatasourceImpl(api, provideFakeSharedPreferences())
+        val result = sut.getRefreshToken()
+
+        Assert.assertEquals(fakeRefreshToken, result)
+    }
+}
