fe63f6431d08670e8c58576b4ea86cb5aa324f2e
Ramos, Yago <138661338+YagoAccenture@users.noreply.github.com>
2025-07-21 12:45:11 +0200

    Mock corrected

--- data/src/test/kotlin/cmm/apps/esmorga/data/mock/UserDataMock.kt
+++ data/src/test/kotlin/cmm/apps/esmorga/data/mock/UserDataMock.kt
@@ -1,15 +1,16 @@
 package cmm.apps.esmorga.data.mock
 
 import cmm.apps.esmorga.data.user.model.UserDataModel
+import cmm.apps.esmorga.domain.user.model.RoleType
 
 object UserDataMock {
 
-    fun provideUserDataModel(name: String = "Hermione", lastName: String = "Granger", email: String = "hermione@dirtyblood.com"): UserDataModel = UserDataModel(
+    fun provideUserDataModel(name: String = "Hermione", lastName: String = "Granger", email: String = "hermione@dirtyblood.com", role: RoleType = RoleType.USER): UserDataModel = UserDataModel(
         dataName = name,
         dataLastName = lastName,
         dataEmail = email,
         dataAccessToken = null,
         dataRefreshToken = null,
-        dataRole = "USER"
+        dataRole = role
     )
 }
