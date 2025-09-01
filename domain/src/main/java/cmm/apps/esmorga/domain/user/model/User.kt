6c5db53996b2ec75554daa71236ae208e1c750af
pablomarinoacn <131967313+pablomarinoacn@users.noreply.github.com>
2025-09-01 16:28:51 +0200

    MOB-357 - Wrong error message on password field on registration screen (#134)
    https://mobilemakers.atlassian.net/browse/MOB-357
    
    ---------
    
    Co-authored-by: Pablo Mariño Cortés <pablomarinoacn@gmail.com>
    Co-authored-by: Github Actions <actions@github.com>

--- domain/src/main/java/cmm/apps/esmorga/domain/user/model/User.kt
+++ domain/src/main/java/cmm/apps/esmorga/domain/user/model/User.kt
@@ -9,7 +9,7 @@ data class User(
     companion object {
         const val NAME_REGEX = "^[a-zA-Z '\\-]{3,100}$"
         const val EMAIL_REGEX = "^(?!.{101})[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
-        const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#\$%^&*()_+'\\-=+/<>]).{8,50}$"
+        const val PASSWORD_REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#\$%^&*()_+'\\-=+/<>.]).{8,50}$"
     }
 }
 
