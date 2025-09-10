2962b48ab07a465dfed6e4a105cc7bbcd5c3dea8
Pablo Mariño Cortés <pablomarinoacn@gmail.com>
2025-09-10 12:07:26 +0200

    update main version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "cmm.apps.esmorga"
         minSdk = 28
         targetSdk = 34
-        versionCode = 201000
-        versionName = "2.1.0"
+        versionCode = 300000
+        versionName = "3.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
