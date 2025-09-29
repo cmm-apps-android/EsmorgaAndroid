ee72f9118b84769e06d14eefb1c99f0ae8a0ebba
Ramos, Yago <138661338+YagoAccenture@users.noreply.github.com>
2025-09-29 09:49:47 +0200

    Added effects and navigations

--- view/src/main/java/cmm/apps/esmorga/view/profileV2/ProfileViewModelV2.kt
+++ view/src/main/java/cmm/apps/esmorga/view/profileV2/ProfileViewModelV2.kt
@@ -6,9 +6,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import cmm.apps.esmorga.domain.user.GetSavedUserUseCaseV2
 import cmm.apps.esmorga.domain.user.LogOutUseCaseV2
+import cmm.apps.esmorga.view.profile.model.ProfileEffect
+import cmm.apps.esmorga.view.profileV2.model.ProfileEffectV2
 import cmm.apps.esmorga.view.profileV2.model.ProfileUiStateV2
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
@@ -19,6 +25,13 @@ class ProfileViewModelV2(
 
     private val _uiState = MutableStateFlow(ProfileUiStateV2())
     val uiState: StateFlow<ProfileUiStateV2> = _uiState.asStateFlow()
+
+    private val _effect: MutableSharedFlow<ProfileEffectV2> = MutableSharedFlow(extraBufferCapacity = 2, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+    val effect: SharedFlow<ProfileEffectV2> = _effect.asSharedFlow()
+
+    init {
+        showUser()
+    }
 
     override fun onStart(owner: LifecycleOwner) {
         super.onStart(owner)
@@ -41,6 +54,16 @@ class ProfileViewModelV2(
         viewModelScope.launch {
             logOutUseCaseV2UseCase.invoke()
             _uiState.value = ProfileUiStateV2(user = null)
+
+            _effect.tryEmit(ProfileEffectV2.NavigateToLogIn)
         }
     }
+    fun navigateTologIn() {
+        _effect.tryEmit(ProfileEffectV2.NavigateToLogIn)
+    }
+
+    fun navigateToChangePassword() {
+        _effect.tryEmit(ProfileEffectV2.NavigateToChangePassword)
+    }
+
 }
