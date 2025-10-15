b4a24471d9d0cd02ecee095ebfdc6f62ad3e3cb9
Ramos, Yago <138661338+YagoAccenture@users.noreply.github.com>
2025-10-15 10:05:49 +0200

    Update CacheHelper.kt

--- data/src/main/java/cmm/apps/esmorga/data/CacheHelper.kt
+++ data/src/main/java/cmm/apps/esmorga/data/CacheHelper.kt
@@ -3,7 +3,7 @@ package cmm.apps.esmorga.data
 
 object CacheHelper {
 
-    const val DEFAULT_CACHE_TTL = 0 // 30 mins
+    const val DEFAULT_CACHE_TTL = 30 * 60 * 1000 // 30 mins
 
     fun shouldReturnCache(dataTime: Long) = dataTime > System.currentTimeMillis() - DEFAULT_CACHE_TTL
 }
