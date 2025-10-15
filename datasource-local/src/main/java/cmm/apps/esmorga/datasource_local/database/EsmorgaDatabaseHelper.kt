30047bc29f3b3eca256229620bc285112b0e8fb0
Ramos, Yago <138661338+YagoAccenture@users.noreply.github.com>
2025-10-15 17:33:46 +0200

    Test DB

--- datasource-local/src/main/java/cmm/apps/esmorga/datasource_local/database/EsmorgaDatabaseHelper.kt
+++ datasource-local/src/main/java/cmm/apps/esmorga/datasource_local/database/EsmorgaDatabaseHelper.kt
@@ -14,6 +14,7 @@ object EsmorgaDatabaseHelper {
     fun getDatabase(context: Context) =
         Room.databaseBuilder(context, EsmorgaDatabase::class.java, DATABASE_NAME)
             .addMigrations(MIGRATION_3_4, MIGRATION_4_5)
+            .fallbackToDestructiveMigration()
             .build()
 
     private val MIGRATION_3_4 = object : Migration(3, 4) {
@@ -25,6 +26,26 @@ object EsmorgaDatabaseHelper {
     private val MIGRATION_4_5 = object : Migration(4, 5) {
         override fun migrate(database: SupportSQLiteDatabase) {
             database.execSQL("DROP TABLE IF EXISTS EventLocalModel")
+            database.execSQL(
+                """
+            CREATE TABLE IF NOT EXISTS EventLocalModel (
+                localId TEXT NOT NULL PRIMARY KEY,
+                localName TEXT NOT NULL,
+                localDate INTEGER NOT NULL,
+                localDescription TEXT NOT NULL,
+                localType TEXT NOT NULL,
+                localImageUrl TEXT,
+                localLocationName TEXT NOT NULL,
+                localLocationLat REAL,
+                localLocationLong REAL,
+                localTags TEXT NOT NULL,
+                localCreationTime INTEGER NOT NULL,
+                localUserJoined INTEGER NOT NULL DEFAULT 0,
+                localCurrentAttendeeCount INTEGER NOT NULL DEFAULT 0,
+                localMaxCapacity INTEGER
+            )
+            """.trimIndent()
+            )
         }
     }
 }
