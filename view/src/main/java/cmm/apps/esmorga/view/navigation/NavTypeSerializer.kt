8a154cb7520e9d1083d9c1102cb9be6c6bab56ee
Andres Lopez <andres.lopez@accenture.com>
2025-10-24 18:04:12 +0200

    MOB-454: Fixed serialization of Events for using in NavGraph

--- view/src/main/java/cmm/apps/esmorga/view/navigation/NavTypeSerializer.kt
+++ view/src/main/java/cmm/apps/esmorga/view/navigation/NavTypeSerializer.kt
@@ -2,19 +2,20 @@ package cmm.apps.esmorga.view.navigation
 
 import android.os.Bundle
 import androidx.navigation.NavType
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import java.net.URLDecoder
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
 inline fun <reified T : Any> serializableType(
     isNullableAllowed: Boolean = false,
     json: Json = Json,
 ) = object : NavType<T>(isNullableAllowed = isNullableAllowed) {
-    override fun get(bundle: Bundle, key: String) =
-        bundle.getString(key)?.let<String, T>(json::decodeFromString)
+    override fun get(bundle: Bundle, key: String): T? = bundle.getString(key)?.let { parseValue(it) }
 
-    override fun parseValue(value: String): T = json.decodeFromString(value)
+    override fun parseValue(value: String): T = json.decodeFromString(URLDecoder.decode(value, StandardCharsets.UTF_8.toString()))
 
-    override fun serializeAsValue(value: T): String = json.encodeToString(value)
+    override fun serializeAsValue(value: T): String = URLEncoder.encode(json.encodeToString(value), StandardCharsets.UTF_8.toString())
 
     override fun put(bundle: Bundle, key: String, value: T) {
         bundle.putString(key, json.encodeToString(value))
