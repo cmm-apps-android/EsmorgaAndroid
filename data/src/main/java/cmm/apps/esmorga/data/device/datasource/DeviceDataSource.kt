1fe487bea1754ce6e62457be37337e42ed52e4fd
Andres Lopez <andres.lopez@accenture.com>
2025-11-04 15:20:03 +0100

    MOB-465: Removed getEnvironment as it is not needed anywhere.

--- data/src/main/java/cmm/apps/esmorga/data/device/datasource/DeviceDataSource.kt
+++ data/src/main/java/cmm/apps/esmorga/data/device/datasource/DeviceDataSource.kt
@@ -1,6 +1,5 @@
 package cmm.apps.esmorga.data.device.datasource
 
-import cmm.apps.esmorga.domain.buildConfig.EsmorgaBuildConfig
 import cmm.apps.esmorga.domain.result.ErrorCodes
 import cmm.apps.esmorga.domain.result.EsmorgaException
 import cmm.apps.esmorga.domain.result.Source
@@ -8,10 +7,6 @@ import cmm.apps.esmorga.domain.result.Source
 interface DeviceDataSource {
 
     suspend fun getDeviceId(): String {
-        throw EsmorgaException(message = "Unsupported operation", source = Source.UNSUPPORTED, code = ErrorCodes.UNSUPPORTED_OPERATION)
-    }
-
-    suspend fun getEnvironment(): EsmorgaBuildConfig.Environment {
         throw EsmorgaException(message = "Unsupported operation", source = Source.UNSUPPORTED, code = ErrorCodes.UNSUPPORTED_OPERATION)
     }
 
